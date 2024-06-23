ad0e9bfeaf1588c6067c836870998334d488e8e5
Kaio <kakacnmendes@gmail.com>
2024-06-23 14:29:10 -0300

    edit cards

--- script.js
+++ script.js
@@ -27,40 +27,64 @@ async function printPersonagem() {
     const data = await response.json();
     const personagens = data.results
 
+    console.log(personagens)
+    // console.log(pagina)
+
     const nomeFiltrado = document.getElementById('input-name').value
+    const btnGo = document.getElementById('btnSearch');
+    const conter = document.getElementById('container');
+
+    
+    
 
     const personagemFiltrado = personagens.filter((item) => {
         return item.name.includes(nomeFiltrado);
     });
 
     personagemFiltrado.map((item)=>{
-        console.log(item.image);
+        // console.log(item.image);
     })
 
-    console.log("#####", personagemFiltrado);
+    
 
-    let container = document.getElementById('container')
+  
 
-    personagens.map((itens) => {
+    
+    const percorrerPersonag = personagens.map((itens) => {
+
+        console.log(itens)
 
         const div = document.createElement('div')
         div.style.border = "1px solid black"
 
-        const name = document.createElement('p')
+        const name = document.createElement('h2')
 
         const img = document.createElement('img')
 
+        // descrição
+        const desc = document.createElement('p')
+
         name.innerText = itens.name
         img.src = itens.image 
+        desc.innerHTML = `
+        <span>Gênero:</span> ${itens.gender} <br>
+        <span>Origem:</span> ${itens.origin.name} <br>
+        <span>Espécie:</span> ${itens.species} <br>
+        <span>Status:</span> ${itens.status} <br>
 
-        // div.appendChild(p)
+
+        `
+
+        conter.appendChild(div)
+        div.appendChild(name)
         div.appendChild(img)
-
-        container.appendChild(div)
+        div.appendChild(desc)
         
     })
+
 
     
 }
 
 printPersonagem()
+
